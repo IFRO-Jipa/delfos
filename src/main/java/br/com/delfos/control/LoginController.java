e44daec99261806dad0412a1d117913cd5ef155c
Leonardo Braz <lhleonardo@hotmail.com>
2016-03-25 12:53:49 -0400

    Otimização dos arquivos

--- src/main/java/br/com/delfos/control/LoginController.java
+++ src/main/java/br/com/delfos/control/LoginController.java
@@ -7,7 +7,6 @@ import java.util.ResourceBundle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.com.delfos.app.LoginApp;
 import br.com.delfos.app.PrincipalApp;
