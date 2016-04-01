4c9c329dcf1116568fbc55c13c3275171b3a3d34
Leonardo Braz <lhleonardo@hotmail.com>
2016-04-01 16:16:44 -0400

    Corrigir problema depois.

--- src/main/java/br/com/delfos/control/FuncionalidadeController.java
+++ src/main/java/br/com/delfos/control/FuncionalidadeController.java
@@ -1,22 +1,34 @@
 package br.com.delfos.control;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import javax.validation.constraints.NotNull;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import br.com.delfos.dao.FuncionalidadeDAO;
+import br.com.delfos.model.Funcionalidade;
 import br.com.delfos.util.AlertBuilder;
 import br.com.delfos.util.ManipuladorDeComponentes;
 import br.com.delfos.util.ManipuladorDeTelas;
+import br.com.delfos.view.FuncionalidadeProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 @Controller
-public class FuncionalidadeController {
+public class FuncionalidadeController implements Initializable {
 	@FXML
 	private Button btnSalvar;
 
@@ -50,6 +62,20 @@ public class FuncionalidadeController {
 	private Button btnNovo;
 
 	@FXML
+	private TableColumn<FuncionalidadeProperty, String> tcFuncionalidade;
+
+	@FXML
+	private TableColumn<FuncionalidadeProperty, Long> tcId;
+
+	@FXML
+	private TableColumn<FuncionalidadeProperty, String> tcChave;
+
+	@Autowired
+	private FuncionalidadeDAO dao;
+
+	private ObservableList<Funcionalidade> funcionalidades = FXCollections.observableArrayList();
+
+	@FXML
 	private void handleBtnPesquisar(ActionEvent event) {
 
 	}
@@ -62,7 +88,7 @@ public class FuncionalidadeController {
 	@FXML
 	private void handleButtonExcluir(ActionEvent event) {
 		if (!txtCodigo.getText().isEmpty()) {
-			// TODO: Lógica para exclusão do registro
+
 		} else {
 			AlertBuilder.information("Selecione um registro para poder excluir");
 		}
@@ -71,9 +97,28 @@ public class FuncionalidadeController {
 	@FXML
 	private void handleButtonSalvar(ActionEvent event) {
 		if (ManipuladorDeComponentes.validaCampos(this)) {
-			// Lógica para salvar o registro
-			// faz nada
-			System.out.println("fim");
+			Funcionalidade f = new Funcionalidade(txtNome.getText(), txtChave.getText(),
+			        txtDescricao.getText(), null);
+			Funcionalidade save = dao.save(f);
+
+			if (save != null) {
+				AlertBuilder.information("Salvo com sucesso");
+//				tbRegistros.getItems().add(save);
+			} else {
+				AlertBuilder.information("Registro não foi salvo... cuidado");
+			}
 		}
 	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		funcionalidades.clear();
+		funcionalidades.addAll(dao.findAll());
+//		tbRegistros.setItems(funcionalidades);
+	}
+
+	private void populaTabela(List<Funcionalidade> funcionalidades) {
+
+	}
+
 }
---
+++ src/main/java/br/com/delfos/view/AbstractTableView.java
@@ -0,0 +1,5 @@
+package br.com.delfos.view;
+
+public class AbstractTableView {
+	
+}
