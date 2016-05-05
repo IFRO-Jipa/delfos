2ebabcd5caf3253ecf2ecbaaed8bce6c095f1985
03248582219 <03248582219@JIPA-LABIN01-04.ifro.local>
2016-05-05 16:51:14 -0400

    Parte do botão NOVO quase completa no PesquisaController

--- src/main/java/br/com/delfos/control/PesquisaController.java
+++ src/main/java/br/com/delfos/control/PesquisaController.java
@@ -4,8 +4,35 @@ import java.awt.event.ActionEvent;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class PesquisaController {
+
+    @FXML
+    private Button removeEspecialistas;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private Button addPesquisador;
+
+    @FXML
+    private TextField txtCodigo;
+
+    @FXML
+    private TextArea txtDescricao;
+
+    @FXML
+    private TableView<?> tbEspecialistas;
+
+    @FXML
+    private ListView<?> listQuestionario;
 
     @FXML
     private Button btnSalvar;
@@ -17,10 +44,10 @@ public class PesquisaController {
     private Button removeQuestionario;
 
     @FXML
-    private Button removeEspecialistas;
+    private DatePicker datePesquisa;
 
     @FXML
-    private Button addPesquisador;
+    private TextField txtLimite;
 
     @FXML
     private Button btnExcluir;
@@ -35,8 +62,19 @@ public class PesquisaController {
     private Button btnNovo;
 
     @FXML
-    void handleButtonNovo(ActionEvent event) {
+    private TableView<?> tbPesquisadores;
 
+    @FXML
+    void handleButtonNovo(ActionEvent event) {
+    	txtNome.setText("");
+    	txtCodigo.setText("");
+    	txtLimite.setText("");
+    	txtDescricao.setText("");
+		tbPesquisadores.getItems().clear();
+		tbEspecialistas.getItems().clear();
+		listQuestionario.getItems().clear();
+		
+		//Falta adicionar a data da pesquisa nesta lista
     }
 
     @FXML
