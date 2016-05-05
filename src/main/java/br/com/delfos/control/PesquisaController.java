6b712c53549841903cbec27c78bb74a08d00b247
03248582219 <03248582219@JIPA-LABIN01-04.ifro.local>
2016-05-05 16:40:52 -0400

    Alterações em PesquisaController

--- src/main/java/br/com/delfos/control/PesquisaController.java
+++ src/main/java/br/com/delfos/control/PesquisaController.java
@@ -1,36 +1,52 @@
 package br.com.delfos.control;
 
-import org.springframework.stereotype.Controller;
+import java.awt.event.ActionEvent;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-@Controller
 public class PesquisaController {
 
-	@FXML
-	private Button btnSalvar;
+    @FXML
+    private Button btnSalvar;
 
-	@FXML
-	private Button btnExcluir;
+    @FXML
+    private Button removePesquisador;
 
-	@FXML
-	private Button btnNovo;
+    @FXML
+    private Button removeQuestionario;
 
-	@FXML
-	void handleButtonNovo(ActionEvent event) {
+    @FXML
+    private Button removeEspecialistas;
 
-	}
+    @FXML
+    private Button addPesquisador;
 
-	@FXML
-	void handleButtonExcluir(ActionEvent event) {
+    @FXML
+    private Button btnExcluir;
 
-	}
+    @FXML
+    private Button addEspecialistas;
 
-	@FXML
-	void handleButtonSalvar(ActionEvent event) {
+    @FXML
+    private Button addQuestionario;
 
-	}
+    @FXML
+    private Button btnNovo;
+
+    @FXML
+    void handleButtonNovo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonExcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonSalvar(ActionEvent event) {
+
+    }
 
 }
