9d3fd20cbafe0c0c612805a3397faa44bf4727ce
MarcoAntonio13 <marcoantonio.tecinf@gmail.com>
2016-05-13 14:28:23 -0400

    Mudando classe para manipular TableView do Cadastro de Questionario

--- src/main/java/br/com/delfos/view/table/QuestionarioPerguntas.java
+++ src/main/java/br/com/delfos/view/table/QuestionarioPerguntas.java
@@ -1,13 +1,15 @@
 package br.com.delfos.view.table;
 
+import br.com.delfos.model.pesquisa.TipoPergunta;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class QuestionarioPerguntas {
 	
 	private final SimpleStringProperty nome;
-	private final SimpleStringProperty tipoPergunta;
+	private final SimpleObjectProperty<TipoPergunta> tipoPergunta;
 	
-	public QuestionarioPerguntas(SimpleStringProperty nome, SimpleStringProperty tipoPergunta) {
+	public QuestionarioPerguntas(SimpleStringProperty nome, SimpleObjectProperty<TipoPergunta> tipoPergunta ) {
 		this.nome = nome;
 		this.tipoPergunta = tipoPergunta;
 	}
@@ -20,8 +22,4 @@ public class QuestionarioPerguntas {
 		return tipoPergunta.get();
 	}
 	
-	
-	
-	
-
 }
