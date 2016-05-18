5e87593e952bebf2caacda0a8ddafcf550144318
MarcoAntonio13 <marcoantonio.tecinf@gmail.com>
2016-05-18 15:53:53 -0400

    Tirando metodos inuteis em PerguntaDAO

--- src/main/java/br/com/delfos/dao/pesquisa/PerguntaDAO.java
+++ src/main/java/br/com/delfos/dao/pesquisa/PerguntaDAO.java
@@ -2,9 +2,6 @@ package br.com.delfos.dao.pesquisa;
 
 
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import br.com.delfos.dao.generic.AbstractDAO;
@@ -16,40 +13,4 @@ import br.com.delfos.repository.pesquisa.PerguntaRepository;
 @Repository
 public class PerguntaDAO extends AbstractDAO<Pergunta, Long, PerguntaRepository>{
 	
-	@Autowired
-	private PerguntaRepository repository;
-
-	public long count() {
-		return repository.count();
-	}
-
-	public void delete(Long arg0) {
-		repository.delete(arg0);
-	}
-	
-
-	public boolean exists(Long arg0) {
-		return repository.exists(arg0);
-	}
-
-	public List<Pergunta> findAll() {
-		return repository.findAll();
-	}
-
-	public Pergunta findOne(Long arg0) {
-		return repository.findOne(arg0);
-	}
-
-	public Pergunta getOne(Long arg0) {
-		return repository.getOne(arg0);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
