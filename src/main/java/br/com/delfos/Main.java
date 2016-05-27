7e92faf3bc437e48f45084d0c8fbbc90e3975297
Leonardo Braz <lhleonardo@hotmail.com>
2016-05-27 11:19:56 -0400

    Preparando a aplicação para compatibilidade de Splash Screen. (vai ficar lindo <3)

--- src/main/java/br/com/delfos/Main.java
+++ src/main/java/br/com/delfos/Main.java
@@ -1,15 +1,29 @@
 package br.com.delfos;
 
 import br.com.delfos.app.LoginApp;
+import br.com.delfos.view.AlertBuilder;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
-	//teste
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		new LoginApp().start(primaryStage);
+		try {
+			// Iniciar a tela de splash
+			primaryStage.addEventHandler(WindowEvent.WINDOW_SHOWING, evt -> {
+				handleAfterLoading(evt);
+			});
+			new LoginApp().start(primaryStage);
+		} catch (Exception ex) {
+			AlertBuilder.error(ex);
+		}
+
+	}
+
+	private void handleAfterLoading(WindowEvent evt) {
+		System.out.println("Terminou de carregar o Spring e conectou com o banco de dados.");
 	}
 
 	public static void main(String[] args) {
