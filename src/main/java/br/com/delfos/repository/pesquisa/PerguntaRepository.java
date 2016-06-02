f824fc29acd1a0f0a416b50107fa8cd8e97efa0d
Marco Antônio <marcoantonio.tecinf@gmail.com>
2016-06-02 17:03:23 -0400

    Adicionando Query ao PerguntaRepository

--- src/main/java/br/com/delfos/repository/pesquisa/PerguntaRepository.java
+++ src/main/java/br/com/delfos/repository/pesquisa/PerguntaRepository.java
@@ -1,11 +1,18 @@
 package br.com.delfos.repository.pesquisa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.delfos.model.pesquisa.Pergunta;
+import br.com.delfos.model.pesquisa.Questionario;
 
 
 @SuppressWarnings("rawtypes")
 public interface PerguntaRepository extends JpaRepository<Pergunta, Long> {
+	
+	@Query("select p.nome p.alternativa_id from Pergunta p inner join Questionario on p.id = Questionario.id where Questionario.id=?")
+	List<Pergunta> findByQuestionario(Questionario questionario);
 
 }
