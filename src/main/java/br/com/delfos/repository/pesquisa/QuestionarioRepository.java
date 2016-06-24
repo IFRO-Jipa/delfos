2c2a4d3d477a838c0660f2b80140c43e753e494d
Leonardo Braz <lhleonardo05@gmail.com>
2016-06-24 13:08:52 -0300

    Criando consulta personalizada.

--- src/main/java/br/com/delfos/repository/pesquisa/QuestionarioRepository.java
+++ src/main/java/br/com/delfos/repository/pesquisa/QuestionarioRepository.java
@@ -1,9 +1,15 @@
 package br.com.delfos.repository.pesquisa;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.delfos.model.basic.Pessoa;
 import br.com.delfos.model.pesquisa.Questionario;
 
 public interface QuestionarioRepository extends JpaRepository<Questionario, Long> {
 
+	@Query(value = "select p.* from pesquisa_especialistas pe join Pesquisa p on p.id = pe.Pesquisa_id where pe.especialistas_id = ?1", nativeQuery = true)
+	List<Questionario> findByEspecialista(Pessoa pessoa);
 }
